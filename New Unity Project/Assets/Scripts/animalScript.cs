24412f4b2b553f23e225928139d5e1017b546245
eYoto <elliot.os121@gmail.com>
2018-11-02 15:48:53 -0400

    init Commit

---
+++ New Unity Project/Assets/Scripts/animalScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class animalScript : MonoBehaviour {
+
+    public GameObject animals;
+
+	public void buttonClick()
+    {
+        if (animals.activeInHierarchy)
+            animals.SetActive(false);
+        else
+            animals.SetActive(true);
+    }
+}
---
+++ New Unity Project/Assets/UI Fader/UIFader.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIFader : MonoBehaviour {
+
+    public CanvasGroup uiElement;
+
+    public void FadeIn()
+    {
+        StartCoroutine(FadeUI(uiElement, uiElement.alpha, 1));
+        uiElement.gameObject.SetActive(true);
+    }
+
+    public void FadeOut()
+    {
+        StartCoroutine(FadeUI(uiElement, uiElement.alpha, 0));
+        if(uiElement.alpha == 0)
+        uiElement.gameObject.SetActive(false);
+    }
+
+    public IEnumerator FadeUI(CanvasGroup indexUI, float start, float end, float lerpTime = 0.5f)
+    {
+        float _timeStartedLerping = Time.time;
+        float timeSinceStarted = Time.time - _timeStartedLerping;
+        float percentageComplete = timeSinceStarted / lerpTime;
+
+        while (true)
+        {
+            timeSinceStarted = Time.time - _timeStartedLerping;
+            percentageComplete = timeSinceStarted / lerpTime;
+
+            float currentValue = Mathf.Lerp(start, end, percentageComplete);
+
+            indexUI.alpha = currentValue;
+
+            if (percentageComplete >= 1) break;
+
+            yield return new WaitForEndOfFrame();
+        }
+    }
+
+
+
+
+}
