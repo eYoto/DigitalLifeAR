4350ac3540c8be8fe08f681b0b7d38e1ec9341de
eYoto <elliot.os121@gmail.com>
2018-11-27 13:30:47 -0500

    got a scav working

--- New Unity Project/Assets/Scripts/scavengerScript.cs
+++ New Unity Project/Assets/Scripts/scavengerScript.cs
@@ -3,19 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using Vuforia;
 
-public class scavengerScript : MonoBehaviour {
-    public TrackableBehaviour myTrackableBehaviour;
-	// Use this for initialization
-	void Start () {
-        myTrackableBehaviour = GetComponent<TrackableBehaviour>();
-        if (myTrackableBehaviour.TrackableName == "Loggerhead")
-        {
-            Debug.Log("k");
-        }
+public class scavengerScript : DefaultTrackableEventHandler {
+    /*public TrackableBehaviour myTrackableBehaviour;*/
+
+
+    public string imageName;
+
+    // Update is called once per frame
+    protected override void Start()
+    {
+        Debug.Log("My turtle is: " + PlayerPrefs.GetInt("LoggerT"));
     }
-	
-	// Update is called once per frame
-	void Update () {
-        
+
+
+    protected override void OnTrackingFound()
+    {
+        PlayerPrefs.SetInt(imageName, 1);
     }
 }
