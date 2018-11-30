22ad9e559fb4fcc80b9e7a3243b11623b00d1399
eYoto <elliot.os121@gmail.com>
2018-11-29 21:31:41 -0500

    Rotate Fix
    better rotate speed

--- New Unity Project/Assets/Scripts/rotateController.cs
+++ New Unity Project/Assets/Scripts/rotateController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class rotateController : MonoBehaviour {
-    private float rotationRate = 3.0f;
+    private float rotationRate = 0.3f;
 
     void Update()
     {
